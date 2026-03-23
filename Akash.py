04864cca47407b7de3230bb37bdb4960bdd03ef9
Akash-190105 <akashakash35714@gmail.com>
2026-03-23 12:32:45 +0530

    Temperature Program

--- Akash.py
+++ Akash.py
@@ -1,1 +1,9 @@
-print("hello nigga")
+Temperature =int(input("Enetr a number:"))
+if Temperature>30:
+    print("Its hot")
+elif Temperature>=15 and Temperature<=30:
+    print("Its plesant")
+elif Temperature<15:
+    print("Its cold")
+else:
+    print("Invalid input")            
